1e34fd8753a027f00867e13aeb877b5cfa3c0106
Nico <nicogaldamez@gmail.com>
2014-08-11 16:56:11 -0300

    Ajuste en el orden

--- app/models/product.rb
+++ app/models/product.rb
@@ -55,7 +55,7 @@ class Product < ActiveRecord::Base
     # Los productos se ordenan por VOTOS - DIAS_PUBLICADO + 1
     result = result.joins("LEFT JOIN votes ON votes.product_id = products.id")
     result = result.select("(count(votes) / (DATE_PART('minute', current_date - products.created_at) + 1))
-                            + 10 - DATE_PART('minute', current_date - products.created_at)
+                            + 14400 - DATE_PART('minute', current_date - products.created_at)
                             , 
                             products.id, products.name, products.created_at,
                             products.url, products.description, products.user_id")
