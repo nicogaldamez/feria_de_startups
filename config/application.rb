5e8cb54c8d1c91a1531d951f05f2be9bc7260a09
Nico <nicogaldamez@gmail.com>
2014-08-15 12:40:55 -0300

    Twitter stuff

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,12 @@ module FeriaDeStartups
     config.assets.compile = true
     
     GA.tracker = "UA-53750124-1"
+    
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
