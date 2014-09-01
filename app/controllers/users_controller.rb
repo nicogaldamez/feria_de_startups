41b6181cff493614a709834c1788057e3372e2d8
Nico <nicogaldamez@gmail.com>
2014-09-01 10:16:03 -0300

    Bug en guardado de mis datos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:email, :receive_notifications)
+      params.require(:user).permit(:email, :receive_notifications, :receive_daily)
     end  
     
     def current_resource
