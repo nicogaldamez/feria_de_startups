581543e7f51d4e631e7081abe1540f8bb23605c7
Nico <nicogaldamez@gmail.com>
2014-09-13 18:10:40 -0300

    Orden de usuarios en listado de usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   
   # -------------------------------------------------
   def index
-    @users = User.all.paginate(page: params[:page], per_page: 20)
+    @users = User.recents.paginate(page: params[:page], per_page: 20)
     respond_to do |format|
       format.js 
       format.html { render 'index', layout: 'admin'}      
