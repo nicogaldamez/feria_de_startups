f160832bb85601a4e660c22a936bc6589c431c85
Nicolás Galdámez <nicolas.galdamez@snappler.com>
2014-10-09 18:28:58 -0300

    Ajuste de tamaño de submodal

--- lib/const.rb
+++ lib/const.rb
@@ -2,6 +2,7 @@
 
 class Const
 
+  ABOUT_US_URL = 'https://medium.com/@feriadestartups/acerca-de-la-feria-de-startups-9b4fbab432d5'
   PRODUCTS_PER_PAGE = 20
 
   # ----------- REPUTACIÓN -------------
